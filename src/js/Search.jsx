c268b61b15a7d9863eff3c1f8c98f32d2b8e2143
Adeel Tariq <adeeltariqagain@gmail.com>
2021-03-10 21:24:29 +0500

    Searches game by name if ID search fails
    (cherry picked from commit 59d72849a46d64f6c6f7d8a7394e49af75ebf694)

--- src/js/Search.jsx
+++ src/js/Search.jsx
@@ -55,7 +55,15 @@ class Search extends React.Component {
           this.queryApi(type, id);
         });
     } else {
-      this.queryApi(type, id);
+      this.queryApi(type, id)
+      .catch((err) => {
+        type = 'game';
+        this.SGDB.searchGame(game.name)
+          .then((gameResp) => {
+            id = gameResp[0].id;
+            this.queryApi(type, id);
+          });
+      });
     }
   }
 
@@ -93,7 +101,7 @@ class Search extends React.Component {
 
     switch (location.state.assetType) {
     case 'horizontalGrid':
-      this.SGDB.getGrids({ type, id }).then((res) => {
+      return this.SGDB.getGrids({ type, id }).then((res) => {
         this.setState({
           isLoaded: true,
           items: res,
@@ -101,7 +109,7 @@ class Search extends React.Component {
       });
       break;
     case 'verticalGrid':
-      this.SGDB.getGrids({ type, id, dimensions: ['600x900'] }).then((res) => {
+      return this.SGDB.getGrids({ type, id, dimensions: ['600x900'] }).then((res) => {
         this.setState({
           isLoaded: true,
           items: res,
@@ -109,7 +117,7 @@ class Search extends React.Component {
       });
       break;
     case 'hero':
-      this.SGDB.getHeroes({ type, id }).then((res) => {
+      return this.SGDB.getHeroes({ type, id }).then((res) => {
         this.setState({
           isLoaded: true,
           items: res,
@@ -117,7 +125,7 @@ class Search extends React.Component {
       });
       break;
     case 'logo':
-      this.SGDB.getLogos({ type, id }).then((res) => {
+      return this.SGDB.getLogos({ type, id }).then((res) => {
         this.setState({
           isLoaded: true,
           items: res,
