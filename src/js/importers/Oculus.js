ff98142c7106e174c696755a8b84ece725e8d89e
Swapnil Devesh <sid.swapnildevesh@gmail.com>
2022-10-05 20:48:37 +0530

    Add log statements

--- src/js/importers/Oculus.js
+++ src/js/importers/Oculus.js
@@ -116,17 +116,21 @@ class Oculus {
       log.info('Import: Started oculus');
 
       this.getOculusLibraryPaths().then(oculusLibraryPaths => {
+        log.info(`Import: Oculus: oculusLibraryPaths: ${oculusLibraryPaths.join(',')}`);
         const games = [];
         const addGamesPromises = [];
 
         oculusLibraryPaths.forEach(oculusLibraryPath => {
           this.getVolumeLetteredPath(oculusLibraryPath).then(volumeLetteredPath => {
+              log.info(`Import: Oculus: volumeLetteredPath generated: ${volumeLetteredPath}`);
               const manifestDir = volumeLetteredPath + "\\Manifests";
               const softwareDir = volumeLetteredPath + "\\Software";
         
               this.getFilesFromPath(manifestDir, '.json.mini').then(filePaths => {
                 filePaths.forEach(fp => {
+                  log.info(`Import: Oculus: filePath found: ${fp}`);
                   let manifest = JSON.parse(fs.readFileSync(manifestDir + "\\" + fp));
+                  log.info(`Import: Oculus: Manifest found: ${fs.readFileSync(manifestDir + "\\" + fp)}`);
                   const exePath = softwareDir + "\\" + manifest.canonicalName + "\\" + manifest.launchFile;
                   const addGame = this.getGameTitle(manifest.appId).then(name => {
                     games.push({
